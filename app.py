bd3ac314685e01f5fa1379a81c00f42cee37ca36
momes <mo.enokida@gmail.com>
2021-05-05 11:27:51 -0700

    step five

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -206,6 +206,33 @@ def profile():
     """Update profile for current user."""
 
     # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    form = UserEditForm(obj=g.user)
+    
+    user = g.user
+
+    if form.validate_on_submit():
+        password = form.password.data
+
+        if not User.authenticate(username=user.username, password=password):
+            flash("Access unauthorized.", "danger")
+            return redirect("/")
+
+        
+        user.username = form.username.data
+        user.email = form.email.data
+        user.image_url = form.image_url.data
+        user.header_url = form.header_url.data
+        user.bio = form.bio.data
+        db.session.commit()
+
+        return redirect(f"/users/{user.id}")
+    
+    return render_template("users/edit.html", form=form)
+
 
 
 @app.route('/users/delete', methods=["POST"])
---
+++ schema.py
@@ -0,0 +1,47 @@
+# USERS
+#                                  Table "public.users"
+#       Column      |  Type   | Collation | Nullable |              Default              
+# ------------------+---------+-----------+----------+-----------------------------------
+#  id               | integer |           | not null | nextval('users_id_seq'::regclass)
+#  email            | text    |           | not null | 
+#  username         | text    |           | not null | 
+#  image_url        | text    |           |          | 
+#  header_image_url | text    |           |          | 
+#  bio              | text    |           |          | 
+#  location         | text    |           |          | 
+#  password         | text    |           | not null | 
+# Indexes:
+#     "users_pkey" PRIMARY KEY, btree (id)
+#     "users_email_key" UNIQUE CONSTRAINT, btree (email)
+#     "users_username_key" UNIQUE CONSTRAINT, btree (username)
+# Referenced by:
+#     TABLE "follows" CONSTRAINT "follows_user_being_followed_id_fkey" FOREIGN KEY (user_being_followed_id) REFERENCES users(id) ON DELETE CASCADE
+#     TABLE "follows" CONSTRAINT "follows_user_following_id_fkey" FOREIGN KEY (user_following_id) REFERENCES users(id) ON DELETE CASCADE
+#     TABLE "messages" CONSTRAINT "messages_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+
+
+# FOLLOWS
+# Table "public.follows"
+#          Column         |  Type   | Collation | Nullable | Default 
+# ------------------------+---------+-----------+----------+---------
+#  user_being_followed_id | integer |           | not null | 
+#  user_following_id      | integer |           | not null | 
+# Indexes:
+#     "follows_pkey" PRIMARY KEY, btree (user_being_followed_id, user_following_id)
+# Foreign-key constraints:
+#     "follows_user_being_followed_id_fkey" FOREIGN KEY (user_being_followed_id) REFERENCES users(id) ON DELETE CASCADE
+#     "follows_user_following_id_fkey" FOREIGN KEY (user_following_id) REFERENCES users(id) ON DELETE CASCADE
+
+#MESSAGES
+#                                        Table "public.messages"
+#   Column   |            Type             | Collation | Nullable |               Default                
+# -----------+-----------------------------+-----------+----------+--------------------------------------
+#  id        | integer                     |           | not null | nextval('messages_id_seq'::regclass)
+#  text      | character varying(140)      |           | not null | 
+#  timestamp | timestamp without time zone |           | not null | 
+#  user_id   | integer                     |           | not null | 
+# Indexes:
+#     "messages_pkey" PRIMARY KEY, btree (id)
+# Foreign-key constraints:
+#     "messages_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+
