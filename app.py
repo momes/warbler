041d52423dbf78cca97f4f9a4e438d7b8cb825c9
momes <mo.enokida@gmail.com>
2021-07-09 18:27:34 -0700

    psql database config

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
+database_url = database_url.replace('postgres://', 'postgresql://')
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
@@ -102,6 +103,7 @@ def signup():
                 image_url=form.image_url.data or User.image_url.default.arg,
             )
             db.session.commit()
+            breakpoint()
 
         except IntegrityError:
             flash("Username already taken", 'danger')
